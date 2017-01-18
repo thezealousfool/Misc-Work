f1b3bb072473540188a8869efe9ce0300c35aa97
VIVEK ROY <VIVEK ROY>
2017-01-19 02:57:44 +0530

    corrected Pratyoy's challenge program

--- C++/C++Learn/pratyoy.cpp
+++ C++/C++Learn/pratyoy.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+typedef long long int Int;
 
-int factor_count(int n)
+Int factor_count(Int n)
 {
-	int count = 0;
-	for(int i = 2; i < n; ++i) {
+	Int count = 0;
+	for(Int i = 2; i < n; ++i) {
 		if(n % 2 == 0) {
 			++count;
 			n /= 2;
@@ -14,15 +15,15 @@ int factor_count(int n)
 
 int main(void)
 {
-	int T;
+	Int T;
 	std::cin >> T;
 	while(T--) {
-		int N, result = 0;
+		Int N, result = 0;
 		std::cin >> N;
 		while(N--) {
-			int a;
+			Int a;
 			std::cin >> a;
-			if(N & 1 == 0)
+			if((N % 2) == 0)
 				result += (factor_count(a) - 1);
 			else
 				result += ((factor_count(a) - 1) * 4);
